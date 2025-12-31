1bdaab90d30ef5d5553619dfcccf48a6df6fcc09
Saqib-802 <msaqib1558@gmail.com>
2025-12-31 16:17:51 +0530

    update app.jsx file and add two more component and nest it also

---
+++ first-react-component/src/components/Gallary.jsx
@@ -0,0 +1,11 @@
+import Profile from "./Profile"
+export default function  Gallary(){
+  return(
+    <section className="gallary-container">
+    <h1>Amazing scientist photoes</h1>
+    <Profile/>
+    <Profile/>
+    <Profile/>
+    </section>
+  )
+}
---
+++ first-react-component/src/components/Profile.jsx
@@ -0,0 +1,7 @@
+export default function Profile(){
+  return(
+    <>
+    <img src="https://media.istockphoto.com/id/2163767458/photo/microscope-research-and-science-with-woman-in-laboratory-for-medical-or-pharmaceutical-report.jpg?s=2048x2048&w=is&k=20&c=cpflf8TSFVXxODniPc4PTXOoYI_ERZNn64-nb8LTMh8=" alt="jams" width={200} height={200} />
+    </>
+  )
+}
